908f71817be2734f3d7d732b97e5147c2c9d7d39
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2022-04-10 13:54:07 +0200

    Updated to v3.2 (relatively substantial)
    UI:
    -removed leading zeroes in hour values <10
    -added conditionals in output that don't display days and hours if the values are 0
    -added mode con to fit window size to content
    --changed title from 'Blender Render Progress Monitor' to 'BRPM' to fit the smaller window and because acronyms are C.O.O.L
    -slight changes to log output
    
    Functionality:
    -Some bug fixes where time values weren't calculated properly
    
    Known issues:
    -In elapsed time calculation (67-73) something is off. I occasionally got 101h instead of 1h, but the issue wasn't constant and I couldn't figure out what it was.
    -In the Math time section, occasionally there would be an error in the lines 89-97. But it wasn't persistent and I couldn't find out what was up.
    My theory is that there is some combination of numbers in the time string that mess up the formulae.
    -I would like to find a way to overwrite echoed lines instead of cls-ing the whole output to reduce flickering and make the code smaller (I wouln't have to repeat echoing the header, oldest file, etc.) but haven't found a way to do that in batch, yet.

--- BlenderRenderProgressMonitor.3.0_TelegramVersion.bat
+++ BlenderRenderProgressMonitor.3.0_TelegramVersion.bat
@@ -1,7 +1,10 @@
 @echo off
+mode con: cols=50 lines=22
+mode con: cols=50 lines=17
+REM not sure why, but it needs two mode con lines to work, but the first one does nothing.
 setlocal
 
-title Blender Render Progress Monitor v3.0 (Telegram)
+title BRPM 3.2
 REM === https://github.com/induna-crewneck
 
 REM ==== ENTER VARIABLES HERE ===============================================================================
@@ -16,25 +19,30 @@ set waittime=30
 set telegrambottoken=4334584910:AAEPmjlh84N62Lv
 set mytelegramid=123456789
 REM =========================================================================================================
+
 :waitfor1
 echo =============================================
 echo ====== Blender Render Progress Monitor ======
+echo ======== github.com/induna-crewneck =========
 echo =============================================
-
 REM ==== Waiting for first frame ============================================================================
-
 if exist %renderpath%\*.%extension% (
-    echo File detected in target folder. Starting script...
+    echo File detected in target folder.
+    echo Starting script...
 ) else (
-    echo No file detected in target folder. Waiting and re-checking...
+    echo No file detected in target folder.
+    echo Waiting 10s and re-checking...
     timeout 10 2> nul
-    cls
     goto waitfor1
+    cls
 )
 REM =========================================================================================================
 
 :loop
-
+cls
+echo =============================================
+echo ====== Blender Render Progress Monitor ======
+echo =============================================
 REM ==== Find oldest and newest files =======================================================================
 for /f "delims=" %%a in ('dir "%renderpath%" /o-d /b') do set oldestfile=%%a
 for /f "delims=" %%a in ('dir "%renderpath%" /od /b') do set newestfile=%%a
@@ -62,6 +70,7 @@ set /A "days=!DateToJDN(Date):Date=%date2:/=%! - !DateToJDN(Date):Date=%date1:/=
 set /A "ss=(((1%time2::=-100)*60+1%-100) - (((1%time1::=-100)*60+1%-100)"
 if %ss% lss 0 set /A "ss+=60*60*24, days-=1"
 set /A "hh=ss/3600+100, ss%%=3600, mm=ss/60+100, ss=ss%%60+100"
+IF "%hh:~1%" == "00" (set hh=0) else (IF %hh:~1% EQU 0 (set hh=0) else (set hh=%hh:~1%))
 REM echo Time elapsed: %days%d %hh:~1%h %mm:~1%min %ss:~1%s
 REM =========================================================================================================
 
@@ -73,9 +82,10 @@ REM echo %renderedframes%/%totalframes% frames rendered
 REM =========================================================================================================
 
 REM ==== Math time ==========================================================================================
-set /a "elapsedseconds=%hh:~1%*3600"+%mm:~1%*60+%ss:~1%
+set /a "elapsedseconds=%hh%*3600"+%mm:~1%*60+%ss:~1%
 set /a "avgrendersecs=%elapsedseconds%/%renderedframes%
 set /a "avgrendermins=%avgrendersecs%/60"
+REM CHECKPOINT_No Errors until here. Some errors sometimes in the following line(s)?
 set /a "avgrendersecs=%avgrendersecs%-(%avgrendermins%*60)"
 set /a "totalsecs=%elapsedseconds%/%renderedframes%*%totalframes%
 set /a "remainingsecstotal=%totalsecs%-%elapsedseconds%
@@ -84,15 +94,36 @@ set /a "remainingdays=%remainingminstotal%/60/24"
 set /a "remaininghours=%remainingminstotal%/60-(%remainingdays%*24)
 set /a "remainingmins=%remainingminstotal%-(%remaininghours%*60)-(%remainingdays%*24*60)"
 set /a "remainingsecs=%remainingsecstotal%-%remainingmins%*60-%remaininghours%*3600-%remainingdays%*24*60*60"
+if "%NewestTime:~0,2%" == "%OldestTime:~0,2%" (set days=0)
 REM =========================================================================================================
 
 REM === Estimated finish =====================================================================================
-set /a "ETAd=%remainingdays%+%NewestTime:~0,2%"
-if %ETAd% LSS 10 set ETAd=0%ETAd%
+for /F "tokens=1-4" %%a in ('date /t') do set "datenow=%%a"
+if %datenow:~8,2% LSS 10 (set daynow=%datenow:~9,1%) else (set daynow=%datenow:~8,2%)
+if %datenow:~5,2% LSS 10 (set monthnow=%datenow:~6,1%) else (set monthnow=%datenow:~5,2%)
+set /a ETAd=%remainingdays%+%daynow%
+if %ETAd% LSS 10 (set ETAd=0%ETAd% 2> nul)
 set /a "ETAh=%remaininghours%+%NewestTime:~11,2%"
 set /a "ETAm=%remainingmins%+%NewestTime:~14,2%"
 set /a "ETAs=%remainingsecs%+%NewestTime:~17,2%"
 REM echo ETA	%ETAd%/%NewestTime:~3,7% %ETAh%:%ETAm%:%ETAs%
+REM ==========================================================================================================
+
+REM === Total render time ====================================================================================
+set /a "totalRenderDays=%days%+%remainingdays%"
+REM set /a "totalRenderHours=%hh%+%remaininghours%"
+REM strings with leading zeroes throw errors (interpreted as oktal, not numerical ffs)
+IF %remaininghours% EQU 0 (IF %hh% EQU 0 (set totalRenderHours=0) else (set totalRenderHours=%hh%)) else (IF %hh% EQU 0 (set totalRenderHours=%remaininghours%) else (set /a "totalRenderHours=%hh%+%remaininghours%"))
+set /a "totalRenderMins=%remainingmins%+%mm:~1%"
+set /a "totalRenderSecs=%remainingsecs%+%ss:~1%"
+if %totalRenderSecs% geq 60 (set /a "totalRenderSecs=%totalRenderSecs%-60")
+rem echo %totalRenderDays%d, %totalRenderHours%h, %totalRenderMins%min, %totalRenderSecs%sec
+REM ==========================================================================================================
+
+REM === cosmetic fixes =======================================================================================
+REM removing leading zero in values <10
+IF %hh% LSS 10 (set /a "hh=%hh%*1")
+IF %totalRenderHours% LSS 10 (set /a "totalRenderHours=%totalRenderHours%*1")
 REM ==========================================================================================================
 
 REM === Display info =========================================================================================
@@ -104,14 +135,22 @@ echo First render:		%oldestfile%
 echo				%OldestTime:~6,4%-%OldestTime:~3,2%-%OldestTime:~0,2%   %OldestTime:~11,8%
 echo Newest render:		%newestfile%
 echo				%NewestTime:~6,4%-%NewestTime:~3,2%-%NewestTime:~0,2%   %NewestTime:~11,8%
-echo Avg frame render time:	%avgrendermins%min %avgrendersecs%s
-echo Time elapsed:		%days%d %hh:~1%h %mm:~1%min %ss:~1%s
-REM echo Total render time:	%totalRenderDays%d %totalRenderHoursleft%h %totalRenderMinsleft%min
-echo Remaining time:		%remainingdays%d %remaininghours%h %remainingmins%min %remainingsecs%s
-echo Estimated completion:	%NewestTime:~6,4%-%NewestTime:~3,2%-%ETAd%   %ETAh%:%ETAm%:%ETAs%
+
+REM echo Avg frame render time:	%avgrendermins%min %avgrendersecs%s
+IF %avgrendermins% EQU 0 (echo Avg frame render time:	%avgrendersecs%s) else (echo Avg frame render time:	%avgrendermins%min %avgrendersecs%s)
+
+REM echo Time elapsed:		%days%d %hh:~1%h %mm:~1%min %ss:~1%s
+IF %days% EQU 0 (IF %hh% EQU 0 (echo Time elapsed:		%mm:~1%min %ss:~1%s) else (echo Time elapsed:		%hh%h %mm:~1%min %ss:~1%s)) else (echo Time elapsed:		%days%d %hh%h %mm:~1%min %ss:~1%s)
+
+REM echo Total render time:	%totalRenderDays%d %totalRenderHours%h %totalRenderMins%min %totalRenderSecs%sec
+IF %totalRenderDays% EQU 0 (IF %totalRenderHours% EQU 0 (echo Total render time:	%totalRenderMins%min %totalRenderSecs%sec) else (echo Total render time:	%totalRenderHours%h %totalRenderMins%min %totalRenderSecs%sec)) else (echo Total render time:	%totalRenderDays%d %totalRenderHours%h %totalRenderMins%min %totalRenderSecs%sec)
+
+REM echo Remaining time:		%remainingdays%d %remaininghours%h %remainingmins%min %remainingsecs%s
+IF %remainingdays% EQU 0 (IF %remaininghours% EQU 0 (echo Remaining time:		%remainingmins%min %remainingsecs%s) else (echo Remaining time:		%remaininghours%h %remainingmins%min %remainingsecs%s)) else (echo Remaining time:		%remainingdays%d %remaininghours%h %remainingmins%min %remainingsecs%s)
+
+REM echo Estimated completion:	%NewestTime:~6,4%-%NewestTime:~3,2%-%ETAd%   %ETAh%:%ETAm%:%ETAs%
 echo Rendered frames:	%renderedframes% / %totalframes% frames
 if %percentagedone% gtr 99 (echo ######################################## %percentagedone%%%) else (
-if %percentagedone% gtr 97 (echo #######################################_ %percentagedone%%%) else (
 if %percentagedone% gtr 95 (echo ######################################__ %percentagedone%%%) else (
 if %percentagedone% gtr 90 (echo ####################################____ %percentagedone%%%) else (
 if %percentagedone% gtr 85 (echo ##################################______ %percentagedone%%%) else (
@@ -132,27 +171,33 @@ if %percentagedone% gtr 15 (echo ######__________________________________ %perce
 if %percentagedone% gtr 10 (echo ####____________________________________ %percentagedone%%%) else (
 if %percentagedone% gtr 5  (echo ##______________________________________ %percentagedone%%%) else (
 if %percentagedone% gtr 1  (echo #_______________________________________ %percentagedone%%%)else (
-echo ________________________________________ %percentagedone%%%))))))))))))))))))))))
+echo ________________________________________ %percentagedone%%%)))))))))))))))))))))
 echo =============================================
 echo.
 echo Refreshing after %waittime% seconds or on keypress...
-echo.
-
 
 REM ==========================================================================================================
-REM Check if done
-IF %renderedFrames% GEQ %totalframes% (goto done)
-
-REM Waiting before refresh
+REM Check if done, otherwise wait
+IF %renderedFrames% GEQ %totalframes% (goto done) else (
 timeout %waittime% > nul
 cls
-goto loop
+goto loop)
 
 :done
-echo Render should be done
+mode con: cols=50 lines=12
+echo =============================================
+echo ====== Blender Render Progress Monitor ======
+echo =============================================
+echo.
+echo                RENDER COMPLETE
+echo.
+echo Rendered frames:	%renderedframes% / %totalframes% frames
+echo Time elapsed:		%days%d %hh%h %mm:~1%min %ss:~1%s
+echo Completed:		%NewestTime:~6,4%-%NewestTime:~3,2%-%NewestTime:~0,2%   %NewestTime:~11,8%
 REM Sending Telegram message
 set telegrammsg=Blender%%20render%%20has%%20finished%%20rendering%%20%renderedframes%%%20frames
 curl "https://api.telegram.org/bot%telegrambottoken%/sendMessage?chat_id=%mytelegramid%&text=%telegrammsg%" > nul
+echo.
 :question
 set /P c=Save log? [Y/N]?
 if /I "%c%" EQU "Y" (goto log)
@@ -160,15 +205,22 @@ if /I "%c%" EQU "N" (goto end)
 goto question
 
 :log
-echo First render:		%oldestfile%	%oldestDate%	%oldestTimeReal%>>%renderpath%\log.txt
-echo Newest render:		%newestfile%	%newestDate%	%newestTimeReal%>>%renderpath%\log.txt
-echo Rendered frames:	%renderedFrames% / %totalframes% Frames (%percentagedone% %%)>>%renderpath%\log.txt
-echo Avg frame render time:	%avgRenderMins%min %avgRenderSecsleft%s>>%renderpath%\log.txt
-echo Time passed:		%dPassed%d %hPassed%h %mPassed%min>>%renderpath%\log.txt
-echo Total render time:	%totalRenderDays%d %totalRenderHoursleft%h %totalRenderMinsleft%min>>%renderpath%\log.txt
-echo Remaining time:		%remainingDays%d %remainingHours%h %remainingMins%min>>%renderpath%\log.txt
-echo log saved in %renderpath%.
+for /F "tokens=1-4" %%a in ('time /t') do set "timenow=%%a"
+echo %datenow%_%timenow% =========================>>%renderpath%\log.txt
+echo First render:		%oldestfile%>>%renderpath%\log.txt
+echo				%OldestTime:~6,4%-%OldestTime:~3,2%-%OldestTime:~0,2%   %OldestTime:~11,8%>>%renderpath%\log.txt
+echo Newest render:		%newestfile%>>%renderpath%\log.txt
+echo				%NewestTime:~6,4%-%NewestTime:~3,2%-%NewestTime:~0,2%   %NewestTime:~11,8%>>%renderpath%\log.txt
+echo Avg frame render time:	%avgrendermins%min %avgrendersecs%s>>%renderpath%\log.txt
+echo Time elapsed:		%days%d %hh%h %mm:~1%min %ss:~1%s>>%renderpath%\log.txt
+echo Total render time:	%totalRenderDays%d %totalRenderHours%h %totalRenderMins%min %totalRenderSecs%sec>>%renderpath%\log.txt
+echo Remaining time:		%remainingdays%d %remaininghours%h %remainingmins%min %remainingsecs%s>>%renderpath%\log.txt
+REM echo Estimated completion:	%NewestTime:~6,4%-%NewestTime:~3,2%-%ETAd%   %ETAh%:%ETAm%:%ETAs%>>%renderpath%\log.txt
+echo Rendered frames:	%renderedframes% / %totalframes% frames>>%renderpath%\log.txt
+echo Output path:	%renderpath%>>%renderpath%\log.txt
+echo Log saved in render path.
 
 :end
-echo Press any key to exit...
-pause > nul
+REM echo Press any key to exit...
+REM pause > nul
+exit
